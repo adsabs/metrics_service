3379acfbcdece096862f073b70077a78ed84edd5
Edwin Henneken <ehenneken@cfa.harvard.edu>
2014-11-03 16:04:15 -0500

    Resources are now determined from code

--- api.py
+++ api.py
@@ -11,7 +11,8 @@ app_blueprint = Blueprint('api', __name__)
 api = Api(app_blueprint)
 
 class Metrics(Resource):
-
+    """computes all metrics on the POST body"""
+    scope = 'oauth:metrics:read'
     def post(self):
         if not request.json or not 'bibcodes' in request.json:
             abort(400)
@@ -25,6 +26,9 @@ class Metrics(Resource):
 
         return jsonify(results)
 
+class PubMetrics(Resource):
+    """Get metrics for a single publication (identified by its bibcode)"""
+    scope = 'oauth:metrics:read'
     def get(self, bibcode):
        try:
            results = generate_metrics(bibcodes=[bibcode])
@@ -34,12 +38,22 @@ class Metrics(Resource):
        return jsonify(results)
 
 class Resources(Resource):
+    """Overview of available resources"""
+    scope = 'oauth:resources:read'
     def get(self):
-      return config.RESOURCES
+        func_list = {}
+        for rule in app.url_map.iter_rules():
+            if rule.endpoint != 'static':
+                func_list[rule.rule] = {'methods':app.view_functions[rule.endpoint].methods,
+                                        'scope': app.view_functions[rule.endpoint].view_class.scope,
+                                        'description': app.view_functions[rule.endpoint].view_class.__doc__,
+                                       }
+        return jsonify(func_list)
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(Metrics, '/metrics', '/metrics/<string:bibcode>')
+api.add_resource(Metrics, '/metrics')
+api.add_resource(PubMetrics, '/metrics/<string:bibcode>')
 api.add_resource(Resources, '/resources')
 
 if __name__ == '__main__':
