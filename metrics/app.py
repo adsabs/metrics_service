08e5d8949f087316f5fe3201d1a887e21f721350
Edwin Henneken <ehenneken@cfa.harvard.edu>
2014-11-05 16:46:15 -0500

    added api key implementation

--- metrics/app.py
+++ metrics/app.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, g
-from views import blueprint, Resources, UnixTime, Metrics, PubMetrics
+from views import blueprint, Resources, UnixTime, PrintArg, ExampleApiUsage, Metrics, PubMetrics
 from flask.ext.restful import Api
+from client import Client
 
 def create_app():
   api = Api(blueprint)
   api.add_resource(Resources, '/resources')
   api.add_resource(UnixTime, '/time')
+  api.add_resource(PrintArg,'/print/<string:arg>')
+  api.add_resource(ExampleApiUsage,'/search')
   api.add_resource(Metrics, '/metrics')
   api.add_resource(PubMetrics, '/metrics/<string:bibcode>')
 
@@ -18,4 +21,5 @@ def create_app():
   except ImportError:
     pass
   app.register_blueprint(blueprint)
+  app.client = Client(app.config['CLIENT'])
   return app
--- metrics/views.py
+++ metrics/views.py
@@ -16,7 +16,7 @@ class Metrics(Resource):
     def post(self):
         if not request.json or not 'bibcodes' in request.json:
             return {'msg': 'no bibcodes found in POST body'}, 400
-        bibcodes = map(str, request.json['bibcode'])
+        bibcodes = map(str, request.json['bibcodes'])
         if len(bibcodes) > current_app.config['MAX_INPUT']:
             return {'msg': 'number of submitted bibcodes exceeds maximum number'}, 400
         try:
@@ -53,3 +53,21 @@ class UnixTime(Resource):
   scopes = ['oauth:sample_application:read','oauth_sample_application:logged_in']
   def get(self):
     return {'now': time.time()}, 200
+
+class PrintArg(Resource):
+  '''Returns the :arg in the route'''
+  scopes = ['oauth:sample_application:read','oauth:sample_application:logged_in'] 
+  def get(self,arg):
+    return {'arg':arg}, 200
+
+class ExampleApiUsage(Resource):
+  '''This resource uses the app.client.session.get() method to access an api that requires an oauth2 token, such as our own adsws'''
+  scopes = ['oauth:sample_application:read','oauth:sample_application:logged_in','oauth:api:search'] 
+  def get(self):
+    r = current_app.client.session.get('http://api.adslabs.org/v1/search')
+    try:
+      r = r.json()
+      return {'response':r, 'api-token-which-should-be-kept-secret':current_app.client.token}, 200
+    except: #For the moment, 401s are not JSON encoded; this will be changed in the future
+      r = r.text
+      return {'raw_response':r, 'api-token-which-should-be-kept-secret':current_app.client.token}, 501
