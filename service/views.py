90ca0ba6f7d921204863ab2b329b6fee57774a8d
Edwin Henneken <ehenneken@cfa.harvard.edu>
2015-05-20 13:40:10 -0400

    made files PEP8-compliant

--- service/views.py
+++ service/views.py
@@ -8,22 +8,26 @@ import sys
 from utils.metrics import generate_metrics
 
 blueprint = Blueprint(
-      'metrics',
-      __name__,
-      static_folder=None,
+    'metrics',
+    __name__,
+    static_folder=None,
 )
 
-allowed_types = ['basic','citations','histograms','indicators','timeseries']
-allowed_histograms = ['publications','reads','downloads','citations']
+allowed_types = [
+    'basic', 'citations', 'histograms', 'indicators', 'timeseries']
+allowed_histograms = ['publications', 'reads', 'downloads', 'citations']
+
 
 class Metrics(Resource):
+
     """computes metrics on the POST body"""
     scopes = []
-    rate_limit = [1000,60*60*24]
-    decorators = [advertise('scopes','rate_limit')]
+    rate_limit = [1000, 60 * 60 * 24]
+    decorators = [advertise('scopes', 'rate_limit')]
+
     def post(self):
         bibcodes = []
-        query    = None
+        query = None
         try:
             include_tori = request.json['tori']
         except:
@@ -43,14 +47,20 @@ class Metrics(Resource):
         if 'bibcodes' in request.json:
             bibcodes = map(str, request.json['bibcodes'])
             if len(bibcodes) > current_app.config.get('METRICS_MAX_SUBMITTED'):
-                return {'Error': 'Unable to get results!', 'Error Info': 'No results: number of submitted bibcodes exceeds maximum number'}, 200
+                return {'Error': 'Unable to get results!',
+                        'Error Info': 'No results: number of submitted \
+                         bibcodes exceeds maximum number'}, 200
             elif len(bibcodes) == 0:
-                return {'Error': 'Unable to get results!', 'Error Info':'No bibcodes found in POST body'}, 200
+                return {'Error': 'Unable to get results!',
+                        'Error Info': 'No bibcodes found in POST body'}, 200
         elif 'query' in request.json:
             query = request.json['query']
         else:
-            return {'Error': 'Unable to get results!', 'Error Info': 'Nothing supplied to calculate metrics on!'}, 200 
-        results = generate_metrics(bibcodes=bibcodes,query=query,tori=include_tori,types=types,histograms=histograms)
+            return {'Error': 'Unable to get results!',
+                    'Error Info': 'Nothing to calculate metrics!'}, 200
+        results = generate_metrics(
+            bibcodes=bibcodes, query=query, tori=include_tori,
+            types=types, histograms=histograms)
         # If the results contain an error message something went boink
         if "Error" in results:
             return results, 500
@@ -58,15 +68,21 @@ class Metrics(Resource):
         if results:
             return results
         else:
-            return {'Error': 'Unable to get results!', 'Error Info': 'No data available to generate metrics'}, 200
+            return {'Error': 'Unable to get results!',
+                    'Error Info': 'No data available to generate metrics'}, 200
+
 
 class PubMetrics(Resource):
+
     """Get metrics for a single publication (identified by its bibcode)"""
     scopes = []
-    rate_limit = [1000,60*60*24]
-    decorators = [advertise('scopes','rate_limit')]
+    rate_limit = [1000, 60 * 60 * 24]
+    decorators = [advertise('scopes', 'rate_limit')]
+
     def get(self, bibcode):
-        results = generate_metrics(bibcodes=[bibcode], types=['basic','histograms'], histograms=['reads','citations'])
+        results = generate_metrics(bibcodes=[bibcode],
+                                   types=['basic', 'histograms'],
+                                   histograms=['reads', 'citations'])
         # If the results contain an error message something went boink
         if "Error" in results:
             return results, 500
@@ -74,4 +90,5 @@ class PubMetrics(Resource):
         if results:
             return results
         else:
-            return {'Error': 'Unable to get results!', 'Error Info': 'No data available to generate metrics'}, 200
+            return {'Error': 'Unable to get results!',
+                    'Error Info': 'No data available to generate metrics'}, 200
